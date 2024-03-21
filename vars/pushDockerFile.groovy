1c1ea74c927e6976e5605673d24dcdd837306f3d
caredh82 <caredh@hotmail.com>
2024-03-21 12:47:21 -0500

    feat: :construction: shared library cloudcamp variables fix push

--- vars/pushDockerFile.groovy
+++ vars/pushDockerFile.groovy
@@ -3,7 +3,7 @@ def call(Map parameters){
     stage ('Push') {
         sh "aws ecr get-login-password --region ${parameters.region} | docker login --username AWS --password-stdin ${parameters.registryURL}"
         sh "docker tag ${parameters.appName}:latest ${parameters.registryURL}/${parameters.appName}:${parameters.appVersion}"
-        sh "docker tag ${parametes.appName}:latest ${parameters.registryURL}/${parameters.appName}:latest"
+        sh "docker tag ${parameters.appName}:latest ${parameters.registryURL}/${parameters.appName}:latest"
         sh "docker push --all-tags ${parameters.registryURL}/${parameters.appName}"
         
     }
